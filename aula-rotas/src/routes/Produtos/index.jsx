c05959bab7f2f9862c1aab220bf2543c9acafc84
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-05 20:26:26 -0300

    Carregando dados do array em produtos.

--- aula-rotas/src/routes/Produtos/index.jsx
+++ aula-rotas/src/routes/Produtos/index.jsx
@@ -1,7 +1,19 @@
-export default function Produtos(){
+import { Link } from "react-router-dom"
+import { listaProdutos } from "../../components/listaProdutos"
 
-    return 
-    <main>
-        <h1>Produtos</h1>
-    </main>
-}
+export default function Produtos()
+{
+
+    return(
+       <main>
+          <h1>Produtos</h1>
+            {listaProdutos.map(prod =>(
+            <div key={prod.id}>
+                <Link to={`/produtos/editar/${prod.id} `}>
+                 Editar o produto: { prod.name}
+                </Link>
+            </div>
+         ))}
+       </main>
+       )
+    }
