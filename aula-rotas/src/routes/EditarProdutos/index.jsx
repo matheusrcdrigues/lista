ff77170cd1570a9ee5353efaab69497957235a58
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-05 16:40:03 -0300

    Add componentes

---
+++ aula-rotas/src/routes/EditarProdutos/index.jsx
@@ -0,0 +1,7 @@
+export default function EditarProduto(){
+
+    return 
+    <main>
+        <h1>Editando Produto</h1>
+    </main>
+}
---
+++ aula-rotas/src/routes/Error/index.jsx
@@ -0,0 +1,7 @@
+export default function Error(){
+
+    return 
+    <main>
+        <h1>Error 404 - Página não encontrada</h1>
+    </main>
+}
