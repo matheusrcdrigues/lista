6b69fccd777f8c5720638404cbdf6cf31c424fb3
matheusrcdrigues@gmail.com <matheusrcdrigues>
2023-09-05 19:11:00 -0300

    config App.jsx + RouterProvider

--- aula-rotas/src/App.jsx
+++ aula-rotas/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Outlet} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,4 +33,11 @@ function App() {
   )
 }
 
+function App(){
+  return (
+    <Outlet/>
+  )
+}
+
 export default App
+
