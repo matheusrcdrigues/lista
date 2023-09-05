a0f4e1852ee49752f07de53b5485a5b6c5d69645
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-05 17:15:16 -0300

    config rotas

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -13,3 +13,34 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+const router = createBrowserRouter([
+  {
+    path: "/" ,
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/" , 
+        element: <Home/>
+    },
+      {
+        path: "/produtos" ,
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos/>
+      },
+      {
+        path: "/antiga",
+        element: <Navigate to="/"/>
+      }
+
+    ]
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
